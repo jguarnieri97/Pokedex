8a664c1038850f5179fc83c4dd29abbe37863f38
UriSch <71355921+UriSch@users.noreply.github.com>
2023-05-09 18:27:42 -0300

    Header.php

--- header.php
+++ header.php
@@ -14,6 +14,13 @@
                         <img src="https://www.pngall.com/wp-content/uploads/4/Pokeball-PNG-Free-Download.png" alt="Logo" width="50em" class="d-inline-block align-text-top">
                         Pokedex
                     </a>
+                    <form class="d-flex">
+                        <label for="exampleFormControlInput1" class="col-form-label">Correo Electronico</label>
+                        <input type="email" class="rounded" placeholder="admin@admin.com">
+                        <label for="exampleFormControlInput1" class="col-form-label">Contrasaeña</label>
+                        <input type="password" class="rounded" placeholder="contraseña">
+                        <button class="btn bg-warning text-white" type="submit">Iniciar Sesion</button>
+                    </form>
                 </div>
             </nav>
         </header>
