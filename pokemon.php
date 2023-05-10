c48388e1eb06cd30a011c2263e10194925f380cf
UriSch <71355921+UriSch@users.noreply.github.com>
2023-05-09 21:53:17 -0300

    Pokemon.phpCSS

--- pokemon.php
+++ pokemon.php
@@ -12,45 +12,43 @@ if (!$conn) {
 $sql = "SELECT * FROM pokemon WHERE nombre LIKE '". $_GET['nombre'] ."'";
 $result = mysqli_query($conn, $sql);
 
-$pokemon = array();
-if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $param = array();
-        $param['id_pokemon'] = $row['id_pokemon'];
-        $param['nombre'] = $row['nombre'];
-        $param['tipo'] = $row['tipo'];
-        $param['foto'] = $row['foto'];
-        $param['descripcion'] = $row['descripcion'];
-        $param['numero'] = $row['numero'];
-        $pokemon[] = $param;
-    }
-}
+$pokemon = mysqli_fetch_assoc($result);
+
 include 'header.php';
 ?>
-<main>
-    <table class="table">
-        <thead>
-        <tr>
-            <th scope="col">Numero</th>
-            <th scope="col">Nombre</th>
-            <th scope="col">Tipo</th>
-            <th scope="col">Descripcion</th>
-            <th scope="col">Foto</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php
-        foreach ($pokemon as $param){
-            echo "<tr>
-                    <td>" . $param['numero'] ."</td>
-                    <td>" . $param['nombre'] ."</td>
-                    <td> <img src=img/type/" . strtoupper($param['tipo']) . ".jpg width='100em'></td>
-                    <td>" . $param['descripcion'] ."</td>
-                    <td><img src=" . $param['foto'] . "alt=" . $param['id_pokemon'] . "></td>";
-        }
-        ?>
-        </tbody>
-    </table>
+<style>
+    .bg-image{
+        background-image: url("https://i.pinimg.com/originals/47/b9/14/47b91495e80426f8b2d419a23c80da59.png");
+        background-size: cover;
+    }
+</style>
+<main class="mb-5">
+    <section class="container mx-auto mt-3">
+        <article class="w-75 mx-auto">
+            <div class="bg-danger p-5 rounded border border-dark border-4">
+                <div class="text-center bg-image p-5 rounded border border-dark border-4">
+                    <?php
+                    echo "<img src=" . $pokemon['foto'] . "alt=" . $pokemon['id_pokemon'] . " width=300em' class='p-5 rounded'>";
+
+                    ?>
+                </div>
+
+            </div>
+            <div class="card bg-dark text-white p-5">
+                <?php
+                echo "
+                    <div class=' d-flex bg-danger p-4 text-center align-items-center justify-content-around rounded'>
+                        <img src=img/type/" . strtoupper($pokemon['tipo']) . ".jpg width='50em'>
+                        <h2 class='pb-0 mb-0'>" . $pokemon['nombre'] ."</h2>
+                        <p class='pb-0 mb-0 fs-4 text-dark fw-bold'>Nro." . $pokemon['numero'] ."</p>
+                    </div>
+                    <p class='mt-5'>" . $pokemon['descripcion'] ."</p>";
+                ?>
+                </tbody>
+            </div>
+        </article>
+    </section>
+
 </main>
 <?php
 include 'footer.php';
